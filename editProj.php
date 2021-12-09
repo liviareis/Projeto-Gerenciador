9f89911cdc6cb24005a1fd6f53132b3466c7a6cd
Lívia dos Reis <39499296+liviareis@users.noreply.github.com>
2021-12-09 12:59:23 -0300

    Update editProj.php

--- editProj.php
+++ editProj.php
@@ -23,48 +23,48 @@
 		</ul>
 	  </div>
 
-  <div id="corpocad">
-	<form method="post">
+	  <div id="corpocad">
+		<form method="post">
 
-	<ul>
-		<h4>Editar Projeto:</h4>
-		<li>Título do Projeto: <input type="text" name="titulo" placeholder="Informe o Título do Projeto" class="formcad"></li>
-		<li>Status: <select name="status"><option value = "analise">Em Análise</option>
-							<option value = "Desenvolvimento">Em desenvolvimento</option>
-							<option value = "Revisão">Em revisão</option>
-							<option value = "Concluido"> Concluido </option>
-					</select></li>
-		<li><input type="submit" value="Enviar" name="enviar" class="formcad"></li>
-	</ul>
+		<ul>
+			<h4>Editar Projeto:</h4>
+			<li>Título do Projeto: <input type="text" name="titulo" placeholder="Informe o Título do Projeto" class="formcad"></li>
+			<li>Status: <select name="status"><option value = "analise">Em Análise</option>
+								<option value = "Desenvolvimento">Em desenvolvimento</option>
+								<option value = "Revisão">Em revisão</option>
+								<option value = "Concluido"> Concluido </option>
+						</select></li>
+			<li><input type="submit" value="Enviar" name="enviar" class="formcad"></li>
+		</ul>
 
-	</form>
-  </div>
-	<?php 
-		if (isset($_POST['enviar'])) {
-			
-			$titulo = $_POST['titulo'];
-			$status = $_POST['status'];
+		</form>
+	   </div>
+		<?php 
+			if (isset($_POST['enviar'])) {
 
-			$conn = mysqli_connect("localhost", "root", "", "gerenciador");
+				$titulo = $_POST['titulo'];
+				$status = $_POST['status'];
+
+				$conn = mysqli_connect("localhost", "root", "", "gerenciador");
 
 
-			if (!empty($titulo) && !empty($status)){
-				$sql = "UPDATE `projeto` SET `status` = '$status' WHERE `projeto`.`titulo` = '$titulo'"; 
-				
-				// executa a consulta
-				if (mysqli_query($conn, $sql) ){
-					// consulta executada com sucesso
-					// exibindo um poupup javascript para informar o usuario
-					echo "<script>alert('Cadastro editado com sucesso!');</script>";
+				if (!empty($titulo) && !empty($status)){
+					$sql = "UPDATE `projeto` SET `status` = '$status' WHERE `projeto`.`titulo` = '$titulo'"; 
+
+					// executa a consulta
+					if (mysqli_query($conn, $sql) ){
+						// consulta executada com sucesso
+						// exibindo um poupup javascript para informar o usuario
+						echo "<script>alert('Cadastro editado com sucesso!');</script>";
+					} else {
+						// houve um erro ao executar a consulta
+						echo "<br>SQL gerado foi: $sql<br>";
+					}
+
 				} else {
-					// houve um erro ao executar a consulta
-					echo "<br>SQL gerado foi: $sql<br>";
+					echo "<p>Por favor, preencha todos os campos!";
 				}
-
-			} else {
-				echo "<p>Por favor, preencha todos os campos!";
 			}
-		}
-	?>
-</body>
+		?>
+	</body>
 </html>
